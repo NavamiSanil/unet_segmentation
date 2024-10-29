522b4ea6ca47d52fefca0df797b6c920bb3273d1
Navami S <navamisunil174@gmail.com>
2024-10-29 13:31:28 +0530

    Update kitti_datamodule.py

--- kitti_data/kitti_datamodule.py
+++ kitti_data/kitti_datamodule.py
@@ -1,9 +1,7 @@
 import os
 import torch
-
 from pytorch_lightning import LightningDataModule
 from kitti_data.kitti_dataset import KittiDataset
-
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 from torch.utils.data.dataset import random_split
@@ -15,7 +13,7 @@ class KittiDataModule(LightningDataModule):
 
     def __init__(
             self,
-            data_dir: str= '/kaggle/input/data-semantics',
+            data_dir: str = '/kaggle/input/data-semantics',
             val_split: float = 0.2,
             test_split: float = 0.1,
             num_workers: int = 16,
@@ -25,7 +23,7 @@ class KittiDataModule(LightningDataModule):
             **kwargs,
     ):
         """
-        Kitti train, validation and test dataloaders.
+        Kitti train, validation, and test dataloaders.
 
         Note: You need to have downloaded the Kitti dataset first and provide the path to where it is saved.
         You can download the dataset here: http://www.cvlibs.net/datasets/kitti/eval_semseg.php?benchmark=semantics2015
@@ -34,21 +32,12 @@ class KittiDataModule(LightningDataModule):
             - 200 samples
             - Each image is (3 x 1242 x 376)
 
-        In total there are 34 classes but some of these are not useful so by default we use only 19 of the classes
+        In total, there are 34 classes but some of these are not useful so by default we use only 19 of the classes
         specified by the `valid_labels` parameter.
 
-        Example::
-
-            from pl_bolts.datamodules import KittiDataModule
-
-            dm = KittiDataModule(PATH)
-            model = LitModel()
-
-            Trainer().fit(model, dm)
-
-        Args::
+        Args:
             data_dir: where to load the data from path, i.e. '/path/to/folder/with/data_semantics/'
-            val_split: size of validation test (default 0.2)
+            val_split: size of validation set (default 0.2)
             test_split: size of test set (default 0.1)
             num_workers: how many workers to use for loading data
             batch_size: the batch size
@@ -66,16 +55,18 @@ class KittiDataModule(LightningDataModule):
                                  std=[0.32064945, 0.32098866, 0.32325324])
         ])
 
-        # split into train, val, test
+        # Split into train, val, test
         kitti_dataset = KittiDataset(self.data_dir, transform=self.default_transforms)
 
         val_len = round(val_split * len(kitti_dataset))
         test_len = round(test_split * len(kitti_dataset))
         train_len = len(kitti_dataset) - val_len - test_len
 
-        self.trainset, self.valset, self.testset = random_split(kitti_dataset,
-                                                                lengths=[train_len, val_len, test_len],
-                                                                generator=torch.Generator().manual_seed(self.seed))
+        self.trainset, self.valset, self.testset = random_split(
+            kitti_dataset,
+            lengths=[train_len, val_len, test_len],
+            generator=torch.Generator().manual_seed(self.seed)
+        )
 
     def train_dataloader(self):
         loader = DataLoader(self.trainset,
